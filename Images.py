bf745493035f78a96c673a68d82ba05596f8ac31
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-07-11 18:00:16 +0530

    Add files via upload

---
+++ Images.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter.ttk import *
+root=Tk()
+root.geometry("455x244")
+photo=PhotoImage(file=r"C:\Users\PK SINGH\Downloads\1.png")
+label=Label(image=photo)
+label.pack()
+root.mainloop()
---
+++ SecongTkinter.py
@@ -0,0 +1,12 @@
+from tkinter import *
+root=Tk()
+#width * height
+root.geometry("444x444")
+# it decide the size
+#to make it fix min size
+#width,height format
+#(x,y) x decide min value y it min fix size
+#x and y are length and breadth
+root.minsize(200,200)
+root.maxsize(1000,800)
+root.mainloop()
